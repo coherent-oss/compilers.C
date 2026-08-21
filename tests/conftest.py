139997a9bbf7a5698d90165384faeaf3e0d4f2c9
Jason R. Coombs <jaraco@jaraco.com>
2026-08-21 14:57:14 -0400

    Restore the suppress_path_mangle test fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,23 @@ def disable_macos_customization(monkeypatch):
     from compilers.common.platform import macos
 
     monkeypatch.setattr(macos, 'customize_compiler', lambda config_vars: None)
+
+
+# from pytest-dev/pytest#363
+@pytest.fixture(scope="session")
+def monkeysession():
+    from _pytest.monkeypatch import MonkeyPatch
+
+    mpatch = MonkeyPatch()
+    yield mpatch
+    mpatch.undo()
+
+
+@pytest.fixture(scope="module")
+def suppress_path_mangle(monkeysession):
+    """
+    Disable the path mangling in the compiler. Workaround for pypa/distutils#169.
+    """
+    from compilers.C import base
+
+    monkeysession.setattr(base.Compiler, '_make_relative', staticmethod(lambda x: x))
